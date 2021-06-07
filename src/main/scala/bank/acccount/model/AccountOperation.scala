eaa9e69665a79da11dcb0267a29f5696ee297588
Victor Garcia <victor.g64r@gmail.com>
2021-06-07 17:53:20 -0500

    Integrate models used in application

---
+++ src/main/scala/bank/acccount/model/AccountOperation.scala
@@ -0,0 +1,13 @@
+package bank.account.model
+
+import com.twitter.util.Future
+
+trait AccountOperation {
+  def balanceAccount(uuid: String): Future[Option[Any]]
+  def createAccount(uuid: String, amount: Double): Future[Either[Exception, Any]]
+  def fillAccount(uuid: String, amount: Double): Future[Either[Exception, Any]]
+  def deleteAccount(uuid: String): Future[Either[Exception, Any]]
+}
+
+final case class AccountTransaction(uuid: String, amount: Double)
+final case class AccountFillRequest(uuid: String, amount: Double)
